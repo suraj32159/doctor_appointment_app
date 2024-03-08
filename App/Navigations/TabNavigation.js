156347c7e8dab8f2efec8cc8c6b564bc768eb3ee
Suraj Singh <suraj.singh@solutionanalysts.com>
2024-03-08 17:34:26 +0530

    doctor

--- App/Navigations/TabNavigation.js
+++ App/Navigations/TabNavigation.js
@@ -3,13 +3,14 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../Screens/Home';
 import Appointment from '../Screens/Appointment';
 import Profile from '../Screens/Profile';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
+    <Tab.Navigator screenOptions={{headerShown:false}}>
+      <Tab.Screen name="Home" component={Home} options={{tabBarIcon:({color,size}) => <FontAwesome name="home" size={24} color="black" />}}/>
       <Tab.Screen name="Appointment" component={Appointment} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
